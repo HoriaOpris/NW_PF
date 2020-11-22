1f27dad939974f667d6bec6631112bdecc2c6044
HoriaOpris <horia.opris@outlook.com>
2020-11-22 17:14:25 +0000

    Implement save to file option

--- main.cpp
+++ main.cpp
@@ -2,13 +2,29 @@
 #include<fstream>
 #include<string>
 
+using namespace std;
+
 int main(void)
 {
-	std::ifstream file("lloyds.csv");
- 	std::string buf((std::istreambuf_iterator<char>(file)),
-                       (std::istreambuf_iterator<char>()));
-                       
-	std::cout << buf;
- 
+	ofstream file;
+	file.open("test.txt");
+	auto in = 0.0;
+	auto cu = 0.0;
+	char save; 
+
+	cout << "Investment: ";
+	cin >> in;
+	
+	cout << "Current account: ";
+	cin >> cu;
+
+	cout << "Do you want to save? y/n: ";
+	cin >> save;
+	
+	if(save == 'y')
+		file << in + cu;
+	
+	file.close();	
+	
 	return 0;
 }
